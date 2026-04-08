b160c298adc7cdf38b8b6dfcb26802a1e92429d1
Mihail <vnukovmisa72@gmail.com>
2026-04-08 21:37:01 +0300

    private func

--- main.go
+++ main.go
@@ -2,39 +2,30 @@ package main
 
 import "fmt"
 
-type multiplier func(int) int
-
-type operation func(int, int) int
-
-func multiplyBy(m int) multiplier {
-	return func(i int) int {
-		return i * m
+func counter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
 	}
 }
 
 func main() {
-	multiplyByTwo := multiplyBy(2)
-	multiplyByThree := multiplyBy(3)
-	fmt.Println(multiplyByTwo(7), multiplyByThree(9))
+	increment := counter()
+	fmt.Println(increment())
+	fmt.Println(increment())
 
-	var perform operation
-	perform = arithmeticOperation("add")
-	fmt.Println(perform(5, 7))
+	numbers := []int{1, 2, 3, 4, 5}
+	squared := sliceOperation(numbers, func(i int) int {
+		return i * i
+	})
+	fmt.Println(squared)
 }
 
-func arithmeticOperation(op string) func(int, int) int {
-	switch op {
-	case "add":
-		return func(i1, i2 int) int {
-			return i1 + i2
-		}
-	case "subtract":
-		return func(i1, i2 int) int {
-			return i1 - i2
-		}
-	default:
-		return func(i1, i2 int) int {
-			return i1 + i2
-		}
+func sliceOperation(s []int, op func(int) int) []int {
+	result := make([]int, len(s))
+	for i, v := range s {
+		result[i] = op(v)
 	}
+	return result
 }
