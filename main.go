989ae6ae301ac8db1ebe8a4f08316443cc407b4b
Mihail <vnukovmisa72@gmail.com>
2026-04-08 21:18:59 +0300

    var1

--- main.go
+++ main.go
@@ -1,51 +1,71 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func double(x int) int {
+	return x * 2
+}
+
+func triple(x int) int {
+	return x * 3
+}
+
+func apply(f func(int) int, x int) int {
+	return f(x)
+}
+
+func multiplyBy(multiplier int) func(int) int {
+	return func(i int) int {
+		return i * multiplier
+	}
+}
 
 func main() {
-	day := "Tuef"
+	multiplyByTwo := multiplyBy(2)
+	multiplyByThree := multiplyBy(3)
+	fmt.Println(multiplyByTwo(7), multiplyByThree(9))
 
-	switch day {
-	case "Mon":
-		fmt.Println("Its monday")
-	case "Tue":
-		fmt.Println("Oh my ga")
-	default:
-		fmt.Println("Idonow")
+	sayHi()
+	fn := fullname("Mr.", "Bug")
+	sayHiToSomeone(fn)
+
+	_, l := fullnameWithLength("Mr.", "Bug")
+	fmt.Println(fn, l)
+
+	s := sum(1, 2, 3, 4, 5)
+	fmt.Println(s)
+
+	nums := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(sum(nums...))
+
+	fiveTime := func(x int) int {
+		return x * 5
 	}
+	result := apply(fiveTime, 5)
+	fmt.Println(result)
+}
 
-	switch wordLen := len(day); wordLen {
-	case 4:
-		fallthrough
-	case 1:
-		fmt.Println(1)
-	case 2:
-		fmt.Println(22)
-	case 3:
-		fmt.Println(333)
+func sum(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
 	}
-	/* for i := 0; i < 5; i++ {
-		if i == 2 {
-			continue
-		}
-		fmt.Println(i)
-	} */
-	/* outerLoop:
-	   	for i := 0; i < 3; i++ {
-	   		for j := 0; j < 3; j++ {
-	   			if i == 1 && j == 1 {
-	   				continue outerLoop
-	   			}
-	   			fmt.Println(i, j)
-	   			if i == 2 && j == 2 {
-	   				goto end
-	   			}
-	   			fmt.Println("watafa")
-	   		}
-	   	}
+	return total
+}
 
-	   end:
-	   	fmt.Println("The end of the program") */
+func fullnameWithLength(firstName string, lastName string) (string, int) {
+	fn := fmt.Sprintf("%s %s", firstName, lastName)
+	return fn, len(fn)
+}
+
+func sayHi() {
+	fmt.Println("Hi!")
+}
+
+func sayHiToSomeone(name string) {
+	fmt.Println("Hi!", name)
+}
+
+func fullname(firstName string, lastName string) string {
+	return fmt.Sprintf("%s %s", firstName, lastName)
 }
